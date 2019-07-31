2a3506f43dc9b5c1b851dc8fbdf5783894b6d52c
Piotr F <hello@piotrf.pl>
2019-07-31 22:19:14 +0200

    Document Icons.

--- packages/docs/catalog/pages.js
+++ packages/docs/catalog/pages.js
@@ -1,14 +1,30 @@
 import Headline from '@storycopter/ui/components/Headline/Headline';
 import IdocProvider from '@storycopter/ui/providers/IdocProvider';
+import { CreditsIcon, PointerIcon, ShareIcon } from '@storycopter/ui/elements';
 
 const pages = [
+  {
+    title: 'Elements',
+    pages: [
+      {
+        title: 'Icons',
+        content: require('./pages/icons.md'),
+        path: '/elements/icons',
+        imports: {
+          CreditsIcon: CreditsIcon,
+          PointerIcon: PointerIcon,
+          ShareIcon: ShareIcon,
+        },
+      },
+    ],
+  },
   {
     title: 'Components',
     pages: [
       {
         title: 'Headline',
         content: require('./pages/headlines.md'),
-        path: '/blocks/headline',
+        path: '/components/headline',
         imports: {
           Headline: Headline,
           ThemeProvider: IdocProvider,
---
+++ packages/docs/catalog/pages/icons.md
@@ -0,0 +1,21 @@
+# Icons
+
+Icons share properties of Material UI’s _Icon_ component. Please refer to the [official docs](https://material-ui.com/components/icons/) for more details and [API options](https://material-ui.com/api/icon/).
+
+```react
+span: 2
+---
+  <CreditsIcon />
+```
+
+```react
+span: 2
+---
+  <PointerIcon />
+```
+
+```react
+span: 2
+---
+  <ShareIcon />
+```
