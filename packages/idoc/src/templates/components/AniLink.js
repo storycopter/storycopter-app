2db01f0bc341a32d65d7939d4cf1df058268021d
Piotr F <hello@piotrf.pl>
2019-11-14 23:40:31 +0100

    Fix AniLink propagating theme.

--- packages/idoc/src/templates/components/AniLink.js
+++ packages/idoc/src/templates/components/AniLink.js
@@ -2,8 +2,8 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink';
 import React from 'react';
 import { withTheme } from '@material-ui/styles';
 
-const CustomAniLink = props => {
-  return <AniLink bg={props.theme.palette.background.accent} cover direction="down" duration={2} {...props} />;
+const CustomAniLink = ({ theme, ...props }) => {
+  return <AniLink bg={theme.palette.background.accent} cover direction="down" duration={2} {...props} />;
 };
 
 export default withTheme(CustomAniLink);
